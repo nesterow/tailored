a632c2ed38728f799d95f3f15ba139468d78fa4b
Anton Nesterov <anton@demiurg.io>
2023-01-24 03:39:17 +0300

    add cached markdown renderer

--- routes/[page].tsx
+++ routes/[page].tsx
@@ -1,19 +1,14 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
-import Marked from "../components/Marked.tsx";
 import Page from "../layouts/Page.tsx";
-
-function sanitizePath(path: string) {
-  return path.replace(/[^a-z0-9@]*/g, "");
-}
+import { renderMdPage } from "../system/page.ts";
 
 /**
- * TODO: May want to cache it somehow in order to avoid reading the file on every request
+ * This is a handler for a route that renders a markdown page.
  */
 export const handler: Handlers = {
   async GET(req, ctx) {
-    const path = "pages/" + sanitizePath(ctx.params.page) + ".md";
     try {
-      const source = await Deno.readTextFile(path);
+      const source = await renderMdPage(ctx.params.page);
       return ctx.render({
         source,
       });
@@ -26,7 +21,10 @@ export const handler: Handlers = {
 export default function MarkedPage(props: PageProps) {
   return (
     <Page>
-      <Marked source={props.data.source} />
+      <section
+        data-marked
+        dangerouslySetInnerHTML={{ __html: props.data.source }}
+      />
     </Page>
   );
 }
