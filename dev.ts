07c614b32760a19878cfc36b8dd1ce95f8e75550
Anton Nesterov <anton@demiurg.io>
2023-02-15 15:46:54 +0300

    fix: dev precompile option to skip fs changes on first run

--- dev.ts
+++ dev.ts
@@ -1,9 +1,13 @@
 #!/usr/bin/env -S deno run -A --watch=static/,routes/
 
 import dev from "$fresh/dev.ts";
-import { watchMdx } from "https://deno.land/x/watch_mdx@v0.0.4/mod.ts";
+import { watchMdx } from "https://deno.land/x/watch_mdx@v0.0.5/mod.ts";
+
+const precompile =
+  Deno.args.findLast((arg) => arg === "--precompile") !== undefined;
 
 watchMdx({
+  precompile,
   compile: async ({ compile, source, output }) => {
     const result = await compile(source.value, {
       providerImportSource: null,
@@ -16,5 +20,9 @@ watchMdx({
     };
   },
 });
+
+if (precompile) {
+  Deno.exit(0);
+}
 
 await dev(import.meta.url, "./main.ts");
