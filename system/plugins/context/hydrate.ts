fee2097814b44ae1c0ac3e5a39338417b9dfcf21
Anton Nesterov <anton@demiurg.io>
2023-02-27 07:03:11 +0300

    fix: handle possible errors on global context hydration

--- system/plugins/context/hydrate.ts
+++ system/plugins/context/hydrate.ts
@@ -3,48 +3,74 @@
 import { options, VNode } from "preact";
 
 declare module "preact" {
-  interface Options {
-    _render?: (vnode: VNode) => void;
-    __r?: (vnode: VNode) => void;
+  interface Options extends Record<string, (v: VNode) => void> {
+    "_render"?: (vnode: VNode) => void;
+    "__r"?: (vnode: VNode) => void;
   }
 
-  interface VNode {
-    __ctx?: Record<string, any>;
-    __c?: Record<string, any>;
-    _component?: Record<string, any>;
+  interface VNode extends Record<string, any> {
+    "__ctx"?: Record<string, any>;
+    "__c"?: Record<string, any>;
+    "_component"?: Record<string, any>;
   }
 }
 
+// Using some privates here, which might be shorted
+// See preact's `mangle.json`
+const ugly = {
+  _render: "__r",
+  _component: "__c",
+  _id: "__c",
+};
+
+const is_ugly = !options._render;
+const _id = is_ugly ? ugly["_id"] : "_id";
+const _render = is_ugly ? ugly["_render"] : "_render";
+const _component = is_ugly ? ugly["_component"] : "_component";
+
+/**
+ * @param Context
+ * @param state
+ */
 export default function hydrate(Context: any, state: { id: string }) {
+  const valid = document.querySelectorAll("#" + state.id).length <= 1;
+  if (!valid) {
+    throw new Error(
+      "Global context:\n Context.Provider must be used once during render." +
+        "The application must use only one global context",
+    );
+  }
+
   const el = document.getElementById(state.id);
   const value = JSON.parse(
     el?.innerHTML || "{}",
   );
+
   const provider = new Context.Provider({ value });
   provider.props = { value };
 
-  const _vnode = options.vnode;
+  const hook = options.vnode;
   options.vnode = (vnode: VNode) => {
     if (typeof vnode.type === "function") {
       vnode.__ctx = {
-        [Context.__c]: provider,
+        [Context[_id]]: provider,
       };
     }
-    _vnode?.(vnode);
+    hook?.(vnode);
   };
 
-  const _hook_name = options._render ? "_render" : "__r"; // if mangled
-  const _render = options[_hook_name];
-  const _component = _hook_name === "_render" ? "_component" : "__c";
-
-  options[_hook_name] = (vnode) => {
-    if (vnode[_component] && vnode.__ctx) {
-      vnode[_component]!.context = {
-        ...vnode[_component]!.context,
+  const render = options[_render];
+  options[_render] = (vnode) => {
+    if (
+      vnode[_component] &&
+      vnode.__ctx
+    ) {
+      vnode[_component].context = {
+        ...vnode[_component].context,
         ...vnode.__ctx,
       };
     }
-    _render?.(vnode);
+    render?.(vnode);
   };
 
   setTimeout(() => {
